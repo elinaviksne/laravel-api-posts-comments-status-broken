8b4b09a2631c53d65cfd5cd6c97b7eab2ed73542
elinaviksne <elina@gmail.com>
2025-04-02 14:20:49 +0300

    fixed policy, can now delete comments

--- app/Policies/CommentPolicy.php
+++ app/Policies/CommentPolicy.php
@@ -4,11 +4,14 @@ namespace App\Policies;
 
 use App\Models\User;
 use App\Models\Comment;
+use Illuminate\Auth\Access\Response;
 
 class CommentPolicy
 {
     public function delete(User $user, Comment $comment)
     {
-        return $user->id === $comment->user()->user_id;
+        return $user->id === $comment->user->id
+            ? Response::allow()
+            : Response::deny("You do not own this comment");
     }
 }
